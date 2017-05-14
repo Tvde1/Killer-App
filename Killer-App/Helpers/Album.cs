a344738afacb9667641a04cd051bab91b2492b19
Tvde1 <tim3345@live.nl>
2017-05-14 09:15:06 +0200

    Added Artist and Album class.

---
+++ Killer-App/Helpers/Album.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Killer_App.Helpers.Providers;
+
+namespace Killer_App.Helpers
+{
+    internal class Album
+    {
+        private Provider _provider;
+
+        public int Id { get; }
+        public string Name { get; }
+
+        public Album(int id, string name, Provider provider)
+        {
+            Id = id;
+            Name = name;
+            _provider = provider;
+        }
+
+        public List<Song> Songs { get; } //TODO: Hook up to
+        public List<Artist> Artists { get; } //TODO: Hook up
+    }
+}
---
+++ Killer-App/Helpers/Artist.cs
@@ -0,0 +1,14 @@
+﻿using Killer_App.Helpers.Providers;
+
+namespace Killer_App.Helpers
+{
+    public class Artist : User
+    {
+        public int ArtistId { get; }
+
+        public Artist(int artistId, User user) : base(user)
+        {
+            ArtistId = artistId;
+        }
+    }
+}
