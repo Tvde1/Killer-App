bc941a87f1813ecf7420ba079338cc38ba914df0
Tvde1 <tim3345@live.nl>
2017-05-14 09:17:54 +0200

    Updated all creating of classes so they get the providers too

--- Killer-App/Helpers/DAL/Contexts/ContextBase.cs
+++ Killer-App/Helpers/DAL/Contexts/ContextBase.cs
@@ -4,13 +4,21 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web.UI;
+using Killer_App.Helpers.Providers;
 
 namespace Killer_App.Helpers.DAL.Contexts
 {
     internal class ContextBase
     {
         private static readonly string ConnectionString = "xd";
-        
+        private Provider _provider;
+
+        public ContextBase(Provider provider)
+        {
+            _provider = provider;
+        }
+
+
         public static DataTable GetData(string query)
         {
             using (var adapter = new SqlDataAdapter(query, ConnectionString))
@@ -21,24 +29,28 @@ namespace Killer_App.Helpers.DAL.Contexts
             }
         }
 
-        public static Song CreateSong(DataRow row)
+        public Song CreateSong(DataRow row)
         {
-            return row == null ? null : new Song((int)row["SongPk"], (string)row["Name"], (TimeSpan)row["Duration"]);
+            return row == null ? null : new Song((int)row["SongPk"], (string)row["Name"], (TimeSpan)row["Duration"], _provider);
         }
 
-        public static Album CreateAlbum(DataRow row)
+        public Album CreateAlbum(DataRow row)
         {
-            return row == null ? null : new Album((int) row["AlbumPk"], (string) row["Name"]);
+            return row == null ? null : new Album((int) row["AlbumPk"], (string) row["Name"], _provider);
         }
 
-        public static User CreateUser(DataRow row)
+        public User CreateUser(DataRow row)
         {
-            
+            return row == null ? null : new User((int)row["UserPk"], (string)row["Username"], (string)row["FirstName"], (string)row["LastName"], (string)row["Email"], _provider);
         }
 
-        public static Artist CreateArtist(DataRow row)
+        public Artist CreateArtist(DataRow row)
         {
-            return  row == null ? null : new Artist((int) row["ArtistPk"], )
+            if (row == null) return null;
+            var userQuery = $"SELECT * FROM [User] WHERE UserPk = {row["UserFk"]}";
+            var data = GetData(userQuery);
+            var user = CreateUser(data.Rows[0]);
+            return new Artist((int) row["ArtistPk"], user);
         }
 
         public static List<T> CreateList<T>(DataTable table, Func<DataRow, T> function)
