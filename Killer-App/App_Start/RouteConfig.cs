488a3c4fb3fb63468b28bea7ad4bc06260903fad
Tvde1 <tim3345@live.nl>
2017-05-19 21:38:54 +0200

    Set default route to login controller.

--- Killer-App/App_Start/RouteConfig.cs
+++ Killer-App/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Killer_App
@@ -16,7 +12,7 @@ namespace Killer_App
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Signin", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
