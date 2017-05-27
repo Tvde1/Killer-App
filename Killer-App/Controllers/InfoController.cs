7add97a143539f737a92d2219803c7f4221cf06a
Tvde1 <tim3345@live.nl>
2017-05-27 22:27:25 +0200

    Added info controller and views

---
+++ Killer-App/Controllers/InfoController.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Mvc;
+using Killer_App.Helpers.Providers;
+
+namespace Killer_App.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: Info
+        public ActionResult Song()
+        {
+            var provider = (Provider)Session["Provider"];
+
+            var id = Request.Params["id"];
+            if (id == null) return RedirectToAction("Index", "Home");
+            
+            var song = provider.SongProvider.FetchSong(id);
+            if (song == null) return RedirectToAction("Index", "Home");
+
+            return View(song);
+        }
+
+        public ActionResult Album()
+        {
+            var provider = (Provider)Session["Provider"];
+
+            var id = Request.Params["id"];
+            if (id == null) return RedirectToAction("Index", "Home");
+
+            var album = provider.AlbumProvider.FetchAlbum(id);
+            if (album == null) return RedirectToAction("Index", "Home");
+
+            return View(album);
+        }
+
+        public ActionResult Artist()
+        {
+            var provider = (Provider)Session["Provider"];
+
+            var id = Request.Params["id"];
+            if (id == null) return RedirectToAction("Index", "Home");
+
+            var artist = provider.ArtistProvider.FetchArtist(id);
+            if (artist == null) return RedirectToAction("Index", "Home");
+            return View(artist);
+        }
+    }
+}
